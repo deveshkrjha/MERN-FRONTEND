00119b52095a8a2702d97ab1c8ecd41bb6c88e4c
Devesh <debu1.jha@gmail.com>
2025-07-09 10:45:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+  return(
+    <div>
+    <h1>Mern Frontend</h1>
+    <Home/>
+    <h3>This is a footer</h3>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home(props){
+  let name="John"
+  return (
+    <>
+    <div>Hello {name}</div>
+    <p>This is a paragraph</p>
+    </>
+  )
+}
