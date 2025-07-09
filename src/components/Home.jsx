80f8465c472f975e443d82d033a01cbc0602fd38
Devesh <debu1.jha@gmail.com>
2025-07-09 11:22:09 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,35 @@
-export default function Home(props){
-  let name="John"
-  return (
-    <>
-    <div>Hello {name}</div>
-    <p>This is a paragraph</p>
-    </>
-  )
+// export default function Home({ age }) {
+//   let name = "John";
+//   if(age>18)return <h2>Welcome</h2>;
+//   else return <h2>Not allowed</h2>;
+// }
+
+// export default function Home({ age }) {
+//   const handleClick=()=>{
+//     alert("Hello");
+//   };
+//   const handleSubmit=(name)=>{
+//     alert(`Hello ${name}`);
+//   };
+// return(
+//   <>
+//   <h2>Hello World</h2>
+//   <button onClick={handleClick}>Click</button>
+//   <button onClick={()=>handleSubmit("John")}>Submit</button>
+//   </>
+// );
+// }
+
+import { useState } from "react";
+export default function Home() {
+  const [score,setScore]=useState(0);
+  const increment=()=>{
+    setScore(score+1);
+  };
+return(
+  <>
+  <p>{score}</p>
+  <button onClick={increment}>Increment Score</button>
+  </>
+);
 }
