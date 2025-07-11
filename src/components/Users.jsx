5df3f4cf25acf9b4b66763ea3c1f688e8b506d9f
Devesh <debu1.jha@gmail.com>
2025-07-11 22:59:50 +0530

    update

---
+++ src/components/Users.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import React from 'react'
+import { useEffect,useState } from 'react';
+import axios from 'axios';
+export default function Users() {
+  const [users,setUsers]=useState([]);
+  const API_URL=import.meta.env.VITE_API_URL;
+  const fetchUsers=async()=>{
+    const
+  }
+  return (
+    <div>Users</div>
+  )
+}
